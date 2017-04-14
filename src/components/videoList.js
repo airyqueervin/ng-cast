f905f8d1ff235a5ded83509b038e02c10acd9d0e
Hack Reactor Student <student@hackreactor.com>
2017-04-14 16:34:11 -0700

    Start to loop example data on page

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,6 +1,11 @@
 angular.module('video-player')
+// .controller('VideoListCtrl', ['$scope', function ($scope) 
+// {
+//   $scope.videos = window.exampleVideoData;
+// }])
 .directive('videoList', function() {
   return {
     templateUrl: 'src/templates/videoList.html'
   };
+
 });
--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,7 @@
 angular.module('video-player')
+.controller('VideoListEntryCtrl', ['$scope', function ($scope) {
+  $scope.videos = window.exampleVideoData;
+}])
 .directive('videoListEntry', function() {
   return {
     templateUrl: 'src/templates/videoListEntry.html'
